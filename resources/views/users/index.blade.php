c0da35749b21cc1ea21492a74b5bd8287edf7812
Yhadel Armin Rojas <yhadelrojas151@gmail.com>
2026-05-25 18:22:48 -0400

    chicos cambie usuario

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,7 +8,7 @@
     <div class="bg-gradient-to-r from-purple-600 to-indigo-600 p-6 rounded-t-xl text-white">
 
         <h2 class="text-2xl font-bold flex items-center gap-2">
-            👤 Lista de Usuarios Emi
+            👤 Lista de Usuarios EMI-RESIDENTES
         </h2>
 
         <p class="text-sm opacity-90">
